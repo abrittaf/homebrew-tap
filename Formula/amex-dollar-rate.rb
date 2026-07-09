0d52a933d0c92d23ff64268552fac44154ffc750
Fernando Abritta <fernando.abritta@gmail.com>
2026-07-09 00:06:53 -0300

    fix: update checksum for public GitHub source archive

--- Formula/amex-dollar-rate.rb
+++ Formula/amex-dollar-rate.rb
@@ -2,7 +2,7 @@ class AmexDollarRate < Formula
   desc "CLI to retrieve the American Express Argentina exchange rate"
   homepage "https://github.com/abrittaf/amex-dollar-rate"
   url "https://github.com/abrittaf/amex-dollar-rate/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "817fbfcff0498fb3b1461dcc165f04c0fc78b5f00083555867ce70c460110fea"
   license "MIT"
 
   depends_on "jq"
